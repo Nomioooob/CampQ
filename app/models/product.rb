3e94196393b4b349217a9112b1e904a9cdb5bda5
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-07-17 16:01:56 +0900

    added validation to models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
+  CATEGORY = [Tent, Camp Kitchen, Sleeping Bags, Hammocks, Backpacks]
   belongs_to :user
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, numericality: { greater_than: 1 }
+  validates :category, inclusion: { in: CATEGORY }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  CITIES = [Tokyo, Osaka, Kyoto, Fukuoka, Sapporo, Hiroshima, Okayama, Nagano]
+  validates :location, inclusion: { in: CITIES }
 end
