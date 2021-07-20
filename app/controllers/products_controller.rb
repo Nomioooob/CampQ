752d23432f2242b23e593fe6bc0659ae1be0a9da
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-07-20 20:51:34 +0900

    added pundit and authorization

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,39 @@
 class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
 
   def index
+    @products = policy_scope(Product)
   end
 
   def show
+    @product = Product.find(params[:id])
+    authorize @product
   end
 
   def new
+    @product = Product.new
+    authorize @product
   end
 
   def create
+    @product = Product.new(product_params)
+    @user = current_user
+    @product.user = @user
+    authorize @product
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render 'new'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :picture, :category)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,12 @@
-puts "Creating products..."
+# puts "Creating products..."
 
-3.times do
-  Product.create(
-  name: "NEMO Forte 35 Sleeping Bag",
-  description: "Updated with ultralight, ultra-compressible FeatherCore™ synthetic insulation technology to provide an excellent blend of warmth, moisture resistance, comfort and packability",
-  price: 1000,
-  category: "Sleeping Bags",
-  picture: "https://thepillmagazine.com/contents/wp-content/uploads/2020/09/ColetteMcerney_Indiancreek_01-1330x888-1.jpg",
-)
-end
-
-3.times do
-  Product.create(
-  name: "Dominion 1p Ultralight Backpacking Tent",
-  description: "Tapered Design creates more usable space so you can sit up and move around in the tent without feeling claustrophobic!",
-  price: 10000,
-  category: "Tent",
-  picture: "https://thepillmagazine.com/contents/wp-content/uploads/2020/09/ColetteMcerney_Indiancreek_01-1330x888-1.jpg",
-)
-end
+# 3.times do
+#   Product.create(
+#   name: "NEMO Forte 35 Sleeping Bag",
+#   description: "Updated with ultralight, ultra-compressible FeatherCore™ synthetic insulation technology to provide an excellent blend of warmth, moisture resistance, comfort and packability",
+#   price: 1000,
+#   user: User.first,
+#   category: "Sleeping Bags",
+#   picture: "https://thepillmagazine.com/contents/wp-content/uploads/2020/09/ColetteMcerney_Indiancreek_01-1330x888-1.jpg",
+# )
+# end
