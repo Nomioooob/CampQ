13f6906ed1ffe0d767f0f613520b10c071ab64e9
Shogo Miyagi <shogomiyagi94@gmail.com>
2021-07-20 20:55:23 +0900

    Created the landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
+    if params[:query].present?
+      @query = params[:query]
+      @products = Product.where("name LIKE ?", "%#{params[:query]}%")
+    else
+      @products = Product.all
+    end
   end
 end
