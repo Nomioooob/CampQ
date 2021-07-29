50f7cc1b05b6ad223fd53dbe17ac1c244aff35a1
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-07-30 01:10:48 +0900

    new tabs

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_134547) do
-
+ActiveRecord::Schema.define(version: 2021_07_29_110542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +38,6 @@ ActiveRecord::Schema.define(version: 2021_07_27_134547) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer "status", default: 1
-
     t.datetime "start_time"
     t.datetime "end_time"
     t.integer "rating", default: 0
