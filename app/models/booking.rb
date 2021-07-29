1afe1b28c9994e460cf5d2bda5f53ab89520e86a
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2021-07-29 21:15:23 +0900

    Booking card logic and style complete

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :product
-  enum status: %i[accepted pending rejected]
+  enum status: [:accepted, :pending, :rejected]
   validates :rating, inclusion: { in: (0..5) }
 
   def pending?
